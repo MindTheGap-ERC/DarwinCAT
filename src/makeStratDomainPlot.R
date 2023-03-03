703cd4426c74e2a1f3e39a5624cb15a3d635f74d
NiklasHohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-03-03 14:35:17 +0100

    + trait name as input, globalized plot vars

--- src/makeStratDomainPlot.R
+++ src/makeStratDomainPlot.R
@@ -1,39 +1,66 @@
-makeStratDomainPlot = function(ageDepthModel,evolutionarySimulations,plotSeaLevel,plotGaps){
-  traitmin=sapply(evolutionarySimulations,min)
-  traitmax=sapply(evolutionarySimulations,max)
-  ymin=min(traitmin,0)
-  ymax=max(traitmax,0)
-  cols=c("seagreen","sienna","lightblue4")
+makeStratDomainPlot = function(ageDepthModel, 
+                               evolutionarySimulations, 
+                               ymin, 
+                               ymax, 
+                               trait_name,
+                               plotSeaLevel = TRUE, 
+                               plot_hiatuses = TRUE){
+
+  
+  # plot framwework
   plot(NULL,
        ylim=range(ageDepthModel$heightRaw),
        xlim=c(ymin,ymax),
        ylab="Height [m]",
        xlab="")
-  mtext("Trait Value",side = 1)
-  lines(y=range(ageDepthModel$heightRaw),x=c(0,0),lwd=0.5,lty=3)
+  
+  mtext(trait_name,side = 1)
+  
+  lines(
+    y=range(ageDepthModel$heightRaw), 
+    x=c(0,0), 
+    lwd=0.5, 
+    lty=3)
+  
+  # plot evo sims in strat
   for (i in 1:length(evolutionarySimulations)){
-  lines(y=ageDepthModel$heightRaw,
-       x=evolutionarySimulations[[i]],
-       type="l",
-       col=cols[i],
-       lwd=2)
+    lines(
+      y=ageDepthModel$heightRaw,
+      x=evolutionarySimulations[[i]],
+      type="l",
+      col=trait_cols[i],
+      lwd=trait_cols[i],
+      lty = trait_ltys[i])
   }
-  if(plotGaps==TRUE){
+  
+  # plot hiatuses in strat
+  if(plot_hiatuses){
     for (i in 1:length(ageDepthModel$hiatusHeights)){
       lines(y=rep(ageDepthModel$hiatusHeights[i],2),
             x=c(ymin-1,ymax+1),
-            col=grey(0.3))
+            col=hiatus_strat_col,
+            lwd = hiatus_strat_lwd,
+            lty = hiatus_strat_lty)
     }
   }
   
-  if (plotSeaLevel==TRUE){
-    lines(y=ageDepthModel$heightRaw,
-          x=seaLevel/max(seaLevel)*0.5*(ymax - ymin) + 0.5*(ymax + ymin),
-          col="blue",
-          lwd=2)
+  if (plotSeaLevel){
+    lines(
+      y=ageDepthModel$heightRaw,
+      x=seaLevel/max(seaLevel)*0.5*(ymax - ymin) + 0.5*(ymax + ymin),
+      col = sl_col,
+      lwd = sl_lwd, 
+      lty = sl_lty)
     
-    mtext("Sea Level",side=3,col="blue")
-    axis(side=3,at=c(ymin,0.5*(ymax + ymin),ymax),labels = c("Low","","High"))
+    mtext(
+      text = "Sea Level", 
+      side = 3, 
+      col = sl_col)
+    
+    axis(
+      side = 3, 
+      at = c(ymin,0.5*(ymax + ymin),ymax),
+      labels = c("Low","","High"))
     
   }
 }
