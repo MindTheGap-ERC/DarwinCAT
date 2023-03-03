ef30bd22bfe04ccf2061a606641cf6c16165e38f
NiklasHohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-03-03 14:35:50 +0100

    + adjustable y axis, global plot opts

--- src/makeTimeDomainPlot.R
+++ src/makeTimeDomainPlot.R
@@ -1,16 +1,32 @@
-makeTimeDomainPlot <- function(evolutionarySimulations, ageDepthModel, plotSeaLevel, plotGaps) {
-  cols <- c("seagreen", "sienna", "lightblue4")
-  traitmin <- sapply(evolutionarySimulations, min)
-  traitmax <- sapply(evolutionarySimulations, max)
-  ymin <- min(traitmin, 0)
-  ymax <- max(traitmax, 0)
+makeTimeDomainPlot <- function(evolutionarySimulations, 
+                               ageDepthModel, 
+                               ymin,
+                               ymax,
+                               trait_name, 
+                               plotSeaLevel = FALSE, plot_time_gaps = FALSE) {
+
+  
+  
+  # plotting framework
   plot(NULL,
-    xlim = c(0, 2),
+    xlim = range(t),
     ylim = c(ymin, ymax),
     xlab = "Time [Ma]",
     ylab = ""
   )
-  if (plotGaps == TRUE) {
+  
+  # plot 0 axis
+  lines(
+    x = range(t),
+    y = c(0, 0),
+    col = zero_axis_col,
+    lty = zero_axis_lty,
+    lwd = zero_axis_lwd
+    
+  )
+  
+  # plot removed time intervals
+  if (plot_time_gaps) {
     for (i in seq_along(ageDepthModel$hiatusTimeList)) {
       hiatusStart <- ageDepthModel$hiatusTimeList[[i]]$hiatusStart
       hiatusEnd <- ageDepthModel$hiatusTimeList[[i]]$hiatusEnd
@@ -19,37 +35,38 @@ makeTimeDomainPlot <- function(evolutionarySimulations, ageDepthModel, plotSeaLe
         xright = hiatusEnd,
         ybottom = ymin,
         ytop = ymax,
-        col = grey(0.7),
+        col = removed_time_col,
         lty = 0
       )
     }
   }
-  lines(
-    x = range(t),
-    y = c(0, 0),
-    lwd = 0.5,
-    lty = 3
-  )
-  mtext("Trait Value",
+  
+  
+  
+  mtext(
+    text = trait_name,
     side = 2
   )
+  
   for (i in seq_along(evolutionarySimulations)) {
     lines(
       x = t,
       y = evolutionarySimulations[[i]],
-      col = cols[i],
-      lwd = 2
+      col = trait_cols[i],
+      lwd = trait_lwds[i],
+      lty = trait_ltys[i]
     )
   }
 
   if (plotSeaLevel == TRUE) {
     lines(t, seaLevel / max(seaLevel) * 0.5 * (ymax - ymin) + 0.5 * (ymax + ymin),
-      col = "blue",
-      lwd = 2
+      col = sl_col,
+      lwd = sl_lwd,
+      lty = sl_lty
     )
     mtext("Sea Level",
       side = 4,
-      col = "blue"
+      col = sl_col
     )
     axis(
       side = 4,
