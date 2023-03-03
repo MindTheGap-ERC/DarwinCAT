d67aad0c29b22d5306aeca4ab0a294f24fd1894c
NiklasHohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-03-03 15:38:26 +0100

    + side panel

--- app.R
+++ app.R
@@ -27,12 +27,162 @@ ui <- navbarPage(
   ),
   tabPanel(
     title = "Modes of Evolution",
-    "Intro to Evo Modes goes here"
+    "Intro to Evo Modes goes here",
+    column(width = 4, 
+    wellPanel(
+      tags$h3("Evolutionary Simulations"),
+      
+      actionButton("refreshSimulations", label = "refresh simulations"),
+      selectInput(
+        inputId = "noOfSims",
+        label = "Number of Simulations",
+        choices = list("1", "2", "3")
+      ),
+      selectInput(
+        inputId = "modeOfEvolution",
+        label = "mode of evolution",
+        choices = list("Random Walk", "Stasis", "Ornstein-Uhlenbeck")
+      ),
+      conditionalPanel(
+        condition = "input.modeOfEvolution == 'Random Walk'",
+        sliderInput(
+          inputId = "parameter1", "Variability sigma",
+          min = 0,
+          max = 4,
+          value = 1,
+          step = 0.1
+        ),
+        sliderInput(
+          inputId = "parameter2", "Drift my",
+          min = -2,
+          max = 2,
+          value = 0,
+          step = 0.1
+        ),
+        sliderInput(
+          inputId = "parameter3", "initial  value",
+          min = -1,
+          max = 1,
+          value = 0,
+          step = 0.1
+        )
+      ),
+      conditionalPanel(
+        condition = "input.modeOfEvolution == 'Stasis'",
+        sliderInput(
+          inputId = "parameter4", "mean value",
+          min = -1,
+          max = 1,
+          value = 0,
+          step = 0.1
+        ),
+        sliderInput("parameter5", "Variance",
+                    min = 0,
+                    max = 2,
+                    value = 1,
+                    step = 0.1
+        )
+      ),
+      conditionalPanel(
+        condition = "input.modeOfEvolution == 'Ornstein-Uhlenbeck'",
+        sliderInput(
+          inputId = "parameter6", "long term mean value mu",
+          min = -2,
+          max = 2,
+          value = 0,
+          step = 0.1
+        ),
+        sliderInput("parameter7", "pressure of selection theta",
+                    min = 0,
+                    max = 10,
+                    value = 1,
+                    step = 0.1
+        ),
+        sliderInput(
+          inputId = "parameter8", "volatility/variability sigma",
+          min = 0,
+          max = 2,
+          value = 1,
+          step = 0.1
+        ),
+        sliderInput(
+          inputId = "parameter9", "initial value",
+          min = -4,
+          max = 4,
+          value = 2,
+          step = 0.1
+        )
+      )
+    ),
+    wellPanel(
+      tags$h3("Plot Options"),
+      sliderInput(
+        inputId = "min_trait_value",
+        label = "y axis minimum",
+        min = -5,
+        max = 0,
+        value = -1,
+        step = 0.1,
+        animate = FALSE
+      ),
+      sliderInput(
+        inputId = "max_trait_value",
+        label = "y axis maximum",
+        min = 0.1,
+        max = 5,
+        value = 1,
+        step = 0.1,
+        animate = FALSE
+      ),
+      textInput(
+        inputId = "trait_name",
+        label = "Trait",
+        value="log10(Body Size)"
+      )
+      
+    )
+    )
   ),
   tabPanel(
     title = "Carbonate Stratigraphy",
-    "Intro to Strat Pal goes here"
+    "Intro to Strat Pal goes here",
+    column(
+      width = 4,
+      sliderInput(
+        inputId = "distFromShore",
+        label = "Distance from Shore",
+        min = 0.1,
+        max = 15,
+        value = 1,
+        step = 0.1,
+        animate = TRUE
+      ),
+      checkboxInput(
+        inputId = "plotSeaLevel",
+        label = "Show sea level",
+        value = FALSE
+      ),
+      checkboxInput(
+        inputId = "plot_time_gaps",
+        label = "Display Gaps in Time",
+        value = FALSE
+      ),
+      checkboxInput(
+        inputId = "plot_hiatuses",
+        label = "Display Hiatuses in Rock",
+        value = FALSE
+      )
+    ),
+    column(
+      width = 8,
+      fluidRow(
+        column(4, plotOutput("stratDomainPlot")),
+        column(8, plotOutput("ageDepthModelPlot"))
+      ),
+      column(8, plotOutput("timeDomainPlot"), offset = 4)
+    )
   ),
+
   tabPanel(
     title = "StratPal",
     column(
@@ -168,7 +318,8 @@ ui <- navbarPage(
         textInput(
           inputId = "trait_name",
           label = "Trait",
-          value="log10(Body Size)")
+          value="log10(Body Size)"
+        )
         
       ),
       
@@ -222,7 +373,7 @@ server <- function(input, output) {
 
   output$timeDomainPlot <- renderPlot({
     makeTimeDomainPlot(
-      evolutionarySimulations(),
+      evolutionarySimulations = evolutionarySimulations(),
       ageDepthModel = ageDepthModel(),
       trait_name = input$trait_name,
       ymax = input$max_trait_value,
@@ -234,8 +385,8 @@ server <- function(input, output) {
 
   output$stratDomainPlot <- renderPlot({
     makeStratDomainPlot(
-      ageDepthModel(),
-      evolutionarySimulations(),
+      ageDepthModel = ageDepthModel(),
+      evolutionarySimulations = evolutionarySimulations(),
       ymax = input$max_trait_value,
       ymin = input$min_trait_value,
       trait_name = input$trait_name,
