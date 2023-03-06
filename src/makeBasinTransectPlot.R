e00c07192cd6407cd3a0256b459e7d29b490005e
NiklasHohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-03-06 09:04:12 +0100

    formatting, comments

--- src/makeBasinTransectPlot.R
+++ src/makeBasinTransectPlot.R
@@ -1,28 +1,30 @@
-makeBasinTransectPlot = function(distanceFromShore) {
-  dist_from_shore_km = as.numeric(distanceFromShore)
-  max_depth_basin_transect = -170
+makeBasinTransectPlot <- function(distanceFromShore) {
+  dist_from_shore_km <- as.numeric(distanceFromShore)
+
+  # make empty plot
   plot(
     NULL,
-       ylim = c(max_depth_basin_transect,0), # max height,
-       xlim = c(0,max_dist_from_shore_km),
-       xlab = "Distance from Shore [km]",
-       ylab = "Depth [m]"
-    )
-  
+    ylim = c(max_depth_basin_transect, 0), # max height,
+    xlim = c(0, max_dist_from_shore_km),
+    xlab = "Distance from Shore [km]",
+    ylab = "Depth [m]"
+  )
+
+  # insert empty image as background
   rasterImage(
-    basin_transect,
-              xleft = 0,
-              xright = max_dist_from_shore_km,
-              ybottom = max_depth_basin_transect, 
-              ytop = 0
-    )
-  
+    image = basin_transect,
+    xleft = 0,
+    xright = max_dist_from_shore_km,
+    ybottom = max_depth_basin_transect,
+    ytop = 0
+  )
+
+  # mark position in basin
   lines(
-    x = rep(dist_from_shore_km,2),
-    y = c(max_depth_basin_transect,0),
+    x = rep(dist_from_shore_km, 2),
+    y = c(max_depth_basin_transect, 0),
     lty = basin_transect_lty,
     lwd = basin_transect_lwd,
     col = basin_transect_col
   )
-       
 }
--- src/makeWheelerDiagram.R
+++ src/makeWheelerDiagram.R
@@ -1,28 +1,31 @@
-makeWheelerDiagram = function(distanceFromShore) {
-  dist_from_shore_km = as.numeric(distanceFromShore)
+makeWheelerDiagram <- function(distanceFromShore) {
   
+  dist_from_shore_km <- as.numeric(distanceFromShore)
+
+  # create empty plot
   plot(
     NULL,
-       ylim = range(t),
-       xlim = c(0,max_dist_from_shore_km),
-       xlab = "Distance from Shore [km]",
-       ylab = "Time [Myr]"
-    )
-  
+    ylim = range(t),
+    xlim = c(0, max_dist_from_shore_km),
+    xlab = "Distance from Shore [km]",
+    ylab = "Time [Myr]"
+  )
+
+  # insert wheeler diagram as background
   rasterImage(
-    wheeler_diagram,
-              xleft = 0,
-              xright = max_dist_from_shore_km,
-              ybottom = 0, 
-              ytop = max(t)
-    )
-  
+    image = wheeler_diagram,
+    xleft = 0,
+    xright = max_dist_from_shore_km,
+    ybottom = 0,
+    ytop = max(t)
+  )
+
+  # mark position in basin
   lines(
-    x = rep(dist_from_shore_km,2),
+    x = rep(dist_from_shore_km, 2),
     y = range(t),
     lty = wheeler_diagram_lty,
     lwd = wheeler_diagram_lwd,
     col = wheeler_diagram_col
   )
-       
 }
